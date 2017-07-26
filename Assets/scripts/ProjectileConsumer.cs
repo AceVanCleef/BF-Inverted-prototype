327b40b518916588a178908ed01f3b3d085d1e92
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-26 16:38:43 +0200

    projectile bounce and get consumed by player

---
+++ Assets/scripts/ProjectileConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileConsumer : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        print("ouch");
+
+        if (col.gameObject.tag == "Projectile")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
