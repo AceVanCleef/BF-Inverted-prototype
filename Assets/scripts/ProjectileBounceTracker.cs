a9925b375f34ddbc1df2cfd38a6acbf597a5e898
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-27 21:24:05 +0200

    added maximum amount of bounces to projectiles

---
+++ Assets/scripts/ProjectileBounceTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBounceTracker : MonoBehaviour {
+
+    /* how often a projectile can bounce off obstacles */
+    public int MaxBounceCount = 9;
+    private int currentBounceCount = 0;
+
+    public int getCurrentBounceCount()
+    {
+        return currentBounceCount;
+    }
+
+    public int getMaxBounceCount()
+    {
+        return MaxBounceCount;
+    }
+
+    public void incrementCurrentBounceCount()
+    {
+        currentBounceCount++;
+    }
+
+
+}
---
+++ Assets/scripts/ProjectileDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDestroyer : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        ProjectileBounceTracker bounceTracker = col.gameObject.GetComponent<ProjectileBounceTracker>();
+
+        if (tag.Equals("Obstacle") && (col.gameObject.tag == "Projectile" || col.gameObject.tag == "EnemyProjectile"))
+        {
+            bounceTracker.incrementCurrentBounceCount();
+        }
+
+        if (bounceTracker.getCurrentBounceCount() >= bounceTracker.getMaxBounceCount() )
+        {
+            Destroy(col.gameObject);
+        }
+
+    }
+}
