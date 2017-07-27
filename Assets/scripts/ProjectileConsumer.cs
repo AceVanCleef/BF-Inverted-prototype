ac495e640eac2baef4ca5382df61c140eb843862
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-27 18:05:42 +0200

    damaging medpack prefabs called xyHazard added

--- Assets/scripts/ProjectileConsumer.cs
+++ Assets/scripts/ProjectileConsumer.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class ProjectileConsumer : MonoBehaviour {
 
+    public int healthRecovered = 5;
+
     void OnCollisionEnter2D(Collision2D col)
     {
         print("ouch");
 
-        if (col.gameObject.tag == "Projectile")
+        if (tag.Equals("Player") && col.gameObject.tag == "Projectile")
         {
+            //regainHealth
+            //GetComponent<UnitHealth>().regainHealth(healthRecovered);
             Destroy(col.gameObject);
         }
+        else if (tag.Equals("Enemy") && col.gameObject.tag == "EnemyProjectile")
+        {
+            //regainHealth
+            //GetComponent<UnitHealth>().regainHealth(healthRecovered);
+            Destroy(col.gameObject);
+        }
+        //regainHealth
+        GetComponent<UnitHealth>().regainHealth(healthRecovered);
     }
 }
