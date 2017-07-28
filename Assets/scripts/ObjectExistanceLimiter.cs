e249176ba489cc06c951f7b384463f962c1830c4
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-28 11:26:32 +0200

    pulse working expect of damage

---
+++ Assets/scripts/ObjectExistanceLimiter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectExistanceLimiter : MonoBehaviour {
+
+    public int maxLifeSpan = 16; //seconds
+
+    void Start()
+    {
+
+        Destroy(gameObject, maxLifeSpan);       //gameObject == this == parent container
+
+    }
+}
